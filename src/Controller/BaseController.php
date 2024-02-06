6bb189e078b9dc49edcfba3f16dbc5e27786b268
ipena03 <pena.ivann03gmail.com>
2024-02-06 15:07:35 +0100

    café

---
+++ src/Controller/BaseController.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+class BaseController extends AbstractController
+{
+ #[Route('/', name: 'app_accueil')]
+ public function index(): Response
+ {
+ return $this->render('base/index.html.twig', [
+
+ ]);
+ }
+ #[Route('/contact', name: 'app_contact')]
+ public function contact(): Response
+ {
+ return $this->render('base/contact.html.twig', [
+
+ ]);
+ }
+}
